e366af839808860fe83144dea57d933706726a58
Sedláček Jan František [SedlacekJ] <sedlacekj@spravazeleznic.cz>
2025-07-10 10:59:45 +0200

    uprava 2548

--- src/main/java/packets/Var/M/M_DUP.java
+++ src/main/java/packets/Var/M/M_DUP.java
@@ -6,10 +6,13 @@ import java.util.ArrayList;
 
 public class M_DUP extends Variables {
 
-    public M_DUP() {
+    public M_DUP()
+    {
+
         super("M_DUP",
                 2,
                 "Příznaky určující, zda je balíza duplikátem jedné ze sousedních balíz.");
+
     }
 
     @Override
@@ -18,8 +21,8 @@ public class M_DUP extends Variables {
         ArrayList<String> s = super.getCombo();
 
         s.set(0, "Samostatná");
-        s.set(1, "> Kopie následující");
-        s.set(2, "< Kopie předcházející");
+        s.set(1, "Kopie následující");
+        s.set(2, "Kopie předcházející");
         s.set(3, "NOT_USED");
 
         return s;
